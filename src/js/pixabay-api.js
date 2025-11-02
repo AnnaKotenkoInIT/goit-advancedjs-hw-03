c9e1cd08798e807c7b392c68c93a7eb072955bba
AnnaKotenkoInIT <anna.kotenko.kiev@gmail.com>
2025-11-02 13:17:04 +0200

    add hw

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,19 @@
+import { API_KEY } from './keys.js';
+
+export default function searchImagesByQuery(query) {
+  const URL = 'https://pixabay.com/api/';
+
+  return fetch(`${URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      iziToast.error({
+        position: 'topRight',
+        message: `${error}`,
+      });
+    });
+}
